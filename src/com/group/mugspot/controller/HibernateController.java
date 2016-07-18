b5e9044df126925ff233caae81eb9b3d80517a2c
Steve Goldman <gutezach@gmail.com>
2016-07-18 16:32:27 -0400

    added working hibernate through spring

--- src/com/group/mugspot/controller/HibernateController.java
+++ src/com/group/mugspot/controller/HibernateController.java
@@ -1,5 +1,28 @@
 package com.group.mugspot.controller;
 
-public class HibernateController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+public class HibernateController {
+	
+	
+	
+	
+	
+	//@RequestMapping("/spring")
+		
+		@RequestMapping("/spring")
+		public ModelAndView showMessage() {
+			DAO dao = new DAO();
+			ModelAndView mv = new ModelAndView("spring");
+			mv.addObject("messag" , dao.getInfo());
+			return mv;
+		
+	}
+	
+	
+	
+	
 }
